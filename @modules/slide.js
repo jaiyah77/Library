16f226765f1c53dbd8b1cfe5857ca6d65a1591ef
문경태(PC) <demoon84@naver.com>
2016-07-02 08:09:15 +0900

    .

--- @modules/slide.js
+++ @modules/slide.js
@@ -1,19 +1,19 @@
-function slide(container, options){
+function slide(container, options) {
 	if(!container.length){
 		return;
 	}
 	
-	var detect = {},
+	var detect = {isPlay: false},
 		config = {start: 0};
 	
 	$.extend(config, options);
 	
-	function init(){
+	function init() {
 		detect.item = container.find(config.item);
 		detect.min = 0;
 		detect.max = detect.item.length - 1;
-		detect.width = container.width();
 		detect.current = config.start;
+		detectSize();
 		
 		if(config.auto){
 			auto();
@@ -21,37 +21,52 @@ function slide(container, options){
 		
 		detect.item.eq(detect.current).addClass('active').css({'left': 0});
 		
-		$(document).on('click', '[data-ctrl="prev"]', function (e){
-			e.preventDefault();
-			prev();
-		});
-		$(document).on('click', '[data-ctrl="next"]', function (e){
-			e.preventDefault();
-			next();
-		});
+		$(document)
+			.on('click', '[data-ctrl="prev"]', function (e) {
+				e.preventDefault();
+				prev();
+			})
+			.on('click', '[data-ctrl="next"]', function (e) {
+				e.preventDefault();
+				next();
+			})
+			.on('click', '[data-ctrl="play"]', function (e) {
+				e.preventDefault();
+				auto();
+			})
+			.on('click', '[data-ctrl="stop"]', function (e) {
+				e.preventDefault();
+				stop();
+			});
+
+		$(window).on('resize', detectSize);
+	}
+
+	function detectSize() {
+		detect.width = container.width();
 	}
 	
-	function resize(){
-		
-	}
-	
-	function next(){
+	function next() {
 		slide(detect.current == detect.max ? detect.min : detect.current + 1);
 	}
 	
-	function prev(){
+	function prev() {
 		slide(detect.current == detect.min ? detect.max : detect.current - 1);
 	}
 	
-	function auto(){
-		// setInterval(next, 1000);
+	function auto() {
+		detect.isPlay = setInterval(next, 1000);
+	}
+
+	function stop() {
+		clearInterval(detect.isPlay);
 	}
 	
-	function direction(num){
+	function direction(num) {
 		return detect.current > num ? 'right' : 'left';
 	}
 	
-	function slide(index, speed){
+	function slide(index, speed) {
 		if(detect.item.is(':animated')){
 			return;
 		}
@@ -74,13 +89,14 @@ function slide(container, options){
 		detect.item.eq(index).addClass('active').css({'left': value});
 		detect.item.eq(index).animate({'left': 0}, speed);
 		
-		detect.item.eq(detect.current).addClass('active').animate({'left': -value}, speed, function (){
+		detect.item.eq(detect.current).animate({'left': -value}, speed, function () {
+			$(this).removeClass('active');
 			detect.current = index;
 			slideEnd();
 		});
 	}
 	
-	function slideEnd(){
+	function slideEnd() {
 		console.log('callback');
 	}
 	
@@ -95,9 +111,8 @@ function slide(container, options){
 	}
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
 	slide($('.slide'), {
-		item: '.item',
-		auto: true
+		item: '.item'
 	});
 });
