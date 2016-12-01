68ddb8021c660bb31fc645040088067cf83258f9
demoon <demoon2016@ncsoft.com>
2016-12-01 17:29:17 +0900

    .

---
+++ horizontalBox2/src/app.js
@@ -0,0 +1,14 @@
+/**
+ * horizontalBox
+ * Created by Moon Kyung tae on 2016-11-23.
+ */
+
+import test from "./modules/test.js";
+
+class horizontalBox {
+	constructor(geometry, materials){
+		console.log(test);
+	}
+}
+
+new horizontalBox();
